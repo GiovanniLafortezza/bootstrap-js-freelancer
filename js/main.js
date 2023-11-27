fd789c9f0e0312c43e9720aaf534b590e729def9
GiovanniLafortezza <recordeknot388@gmail.com>
2023-11-27 11:03:48 +0100

    step 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+let arrayCoupon = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
 function calculateOffer(event) {
     event.preventDefault();
@@ -13,7 +13,19 @@ function calculateOffer(event) {
     totalOffer = totalOffer * hoursRequested;
     console.log(totalOffer);
 
-    document.getElementById("price").innerHTML = totalOffer;
+    let textInDiscountCoupon = document.getElementById("discount-code").value;
+
+    for(i = 0; i < arrayCoupon.length; i++) {
+        if(arrayCoupon[i] == textInDiscountCoupon) {
+            totalOffer = totalOffer - (totalOffer * 25 / 100);
+            arrayCoupon.pop(i);
+            console.log(arrayCoupon);
+        } else {
+            alert("Codice non valido, sconto non verrà applicato");
+        }
+    }
+
+    document.getElementById("price").innerHTML = totalOffer.toFixed(2);
 
     
 }
